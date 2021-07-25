4f3c412c3574b6c331c56f0015aac2f41356b237
Leeharim-korean <hw05165@naver.com>
2021-07-25 19:37:35 +0900

    1.6 Prisma Client

--- server.js
+++ server.js
@@ -1,32 +1,42 @@
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
 
+const client = new PrismaClient();
+
 const typeDefs = gql`
-    type Movie {
-        title: String
-        year: Int
-    }
-    type Query {
-        movies: [Movie]
-        movie: Movie
-    }
-    type Mutation {
-        createMovie(title: String!): Boolean
-        deleteMovie(title: String!): Boolean
+  type Movie {
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    creatdAt: String!
+    updatedAt: String!
+  }
+  type Query {
+    movies: [Movie]
+    movie(id: Int!): Movie
+  }
+  type Mutation {
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id: String!): Boolean
   }
 `;
 
 const resolvers = {
     Query: {
-        movies: () => [],
-        movie: () => ({ title: "Hello", year: 2021 }),
+        movies: () => client.movie.findMany(),
+        movie: (_, { id }) => ({ title: "Hello", year: 2021 }),
     },
     Mutation: {
-        createMovie: (_, { title }) => {
-            console.log(title);
-            return true;
-        },
-        deleteMovie: (_, { title }) => {
-            console.log(title);
+        createMovie: (_, { title, year, genre }) =>
+            client.movie.create({
+                data: {
+                    title,
+                    year,
+                    genre,
+                },
+            }),
+        deleteMovie: (_, { id }) => {
             return true;
         },
     },
