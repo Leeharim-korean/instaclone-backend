d17e16df86741fca8d6cfced90fc03ca667b33ff
Leeharim-korean <hw05165@naver.com>
2021-07-26 15:56:24 +0900

    1.12 Recep
    to restart remove the testing database and prisma files

--- prisma/migrations/20210725104942_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `creatdAt` on the `Movie` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Movie" DROP COLUMN "creatdAt",
-ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
