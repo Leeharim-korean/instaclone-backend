a7dbee0d979cae40a7fa5b56d9fb76c7a719fd53
Leeharim-korean <hw05165@naver.com>
2021-07-27 14:32:29 +0900

    2.4 login

--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -10,6 +10,11 @@ type User {
     createdAt: String!
     updatedAt: String!
 }
+type LoginResult {
+    ok: Boolean!
+    token: String
+    error: String
+}
 type Mutation {
     createAccount(
         firstName: String!
@@ -18,6 +23,7 @@ type Mutation {
         email: String!
         password: String!
         ): User
+        login(username: String!, password: String!): LoginResult!
     }
     type Query {
         seeProfile(username: String!): User
