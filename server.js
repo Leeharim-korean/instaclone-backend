04180c348748c57572a2af70c5fd3dd6634770d7
Leeharim-korean <hw05165@naver.com>
2021-07-28 15:54:02 +0900

    2.10 authentication part Three

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
+import { getUser } from "./users/users.utils";
 
 const server = new ApolloServer({
   schema,
-  context: {
-    "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI3Mzg4NjQ0fQ.5HUzC1ZSTpww1qU__ChGj1fo04UGFfB_VDCm5xnkAXc"
+  context: async ({ req }) => {
+    return {
+      loggedInUser: await getUser(req.headers.token),
+    };
   },
 });
 
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -7,16 +7,15 @@ export default {
         editProfile: async (
             _,
             { firstName, lastName, username, email, password: newPassword },
-            { token }
+            { loggedInUser }
         ) => {
-            const { id } = await jwt.verify(token, process.env.SECRET_KEY);
             let uglyPassword = null;
             if (newPassword) {
                 uglyPassword = await bcrypt.hash(newPassword, 10);
             }
             const updatedUser = await client.user.update({
                 where: {
-                    id,
+                    id: loggedInUser.id,
                 },
                 data: {
                     firstName,
@@ -39,4 +38,3 @@ export default {
         },
     },
 };
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjI3Mzg4NjQ0fQ.5HUzC1ZSTpww1qU__ChGj1fo04UGFfB_VDCm5xnkAXc
