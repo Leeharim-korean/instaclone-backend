031aaaef537dbf637eb66d9b0b8c2301f76d5c5f
Leeharim-korean <hw05165@naver.com>
2021-08-15 21:31:12 +0900

    2.29 Searching Users

---
+++ users/searchUsers/searchUsers.resolvers.js
@@ -0,0 +1,31 @@
+import client from "../../client";
+
+export default {
+    Query: {
+        searchUsers: async (_, { keyword }) =>
+            client.user.findMany({
+                where: {
+                    OR: [
+                        {
+                            username: {
+                                startsWith: keyword.toLowerCase(),
+                            },
+                        },
+                        {
+                            firstName: {
+                                startsWith: keyword.toLowerCase(),
+                            },
+                        },
+                        {
+                            lastName: {
+                                startsWith: keyword.toLowerCase(),
+                            }
+                        }
+                    ],
+                },
+                take: 5,
+                skip: lastId ? 1 : 0,
+                ...(lastId && { cursor: { id: lastId } }),
+            }),
+    },
+};
