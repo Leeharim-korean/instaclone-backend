8f282e9b82b38cb860cc1df83b7c1c348735e282
Leeharim-korean <hw05165@naver.com>
2021-08-17 12:46:54 +0900

    3.3 Upload Photo part Two

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,3 +1,4 @@
+import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
@@ -7,7 +8,25 @@ export default {
                 if (caption) {
                     /// parse caption
                     // get or create Hashtags
+                    const hashtags = caption.match(/#[\w]+/g);
+                    console.log(hashtags);
                 }
+                client.photo.create({
+                    data: {
+                        file,
+                        caption,
+                        hashtags: {
+                            connectOrCreate: {
+                                where: {
+                                    hashtag: "#food",
+                                },
+                                create: {
+                                    hashtag: "#food",
+                                }
+                            }
+                        }
+                    }
+                })
                 // save the photo WITH the parsed hashtags
                 // add the photo to the hashtags
             }
