f71c1324cbada09042dafc9012ecc566b7b0cacc
Leeharim-korean <hw05165@naver.com>
2021-08-23 17:05:16 +0900

    change Mutations results

--- users/login/login.typeDefs.js
+++ users/login/login.typeDefs.js
@@ -1,10 +1,6 @@
 import { gql } from "apollo-server";
 
 export default gql`
-type MutationResponse {
-  ok: Boolean!
-  error: String
-}
   type Mutation {
     login(username: String!, password: String!): MutationResponse!
   }
--- users/seeProfile/seeProfile.typeDefs.js
+++ users/seeProfile/seeProfile.typeDefs.js
@@ -1,10 +1,6 @@
 import { gql } from "apollo-server";
 
 export default gql`
-type MutationResponse {
-  ok: Boolean!
-  error: String
-}
   type Query {
     seeProfile(username: String!): User
   }
