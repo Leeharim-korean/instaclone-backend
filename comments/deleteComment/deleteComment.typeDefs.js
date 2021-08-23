aa8344da9e091a90f22f4eab7e0db4f482a6913a
Leeharim-korean <hw05165@naver.com>
2021-08-23 16:50:01 +0900

    I don't know

--- comments/deleteComment/deleteComment.typeDefs.js
+++ comments/deleteComment/deleteComment.typeDefs.js
@@ -1,6 +1,10 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
+type MutationResponse {
+    ok: Boolean!
+    error: String
+}
     type Mutation {
         deleteComment(id: Int!): MutationResponse!
     }
--- shared/shared.utils.js
+++
@@ -1,8 +0,0 @@
-import AWS from "aws-sdk";
-
-AWS.config.update({
-    credentials: {
-        accessKeyId: process.env.AWS_KEY,
-        secretAccessKey: process.env.AWS_SECRET
-    }
-});
